fa97ccb4797868b65cd89131f2284d34b13f03e3
Swati Gakhar <swatigakhar@192.168.1.3>
2022-03-02 16:06:15 +0530

    Adding files for student assignment list

---
+++ engageAutomation/pages/engageExperienceApp/AssignmentListDetailsPage_student.page.js
@@ -0,0 +1,148 @@
+"use strict";
+var action = require('../../core/actionLibrary/baseActionLibrary.js')
+var selectorFile = jsonParserUtil.jsonParser(selectorDir)
+var appShellPage = require('./appShell.page.js')
+
+module.exports = {
+    assignmentDetailPageTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.assignmentDetailPageTitle,
+    assignmentDetailPageSubtitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.assignmentDetailPageSubtitle,
+    dueAssignmentBtn: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.dueAssignmentBtn,
+    dueAssignmentCount: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.dueAssignmentCount,
+    due_noAssignmentTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.due_noAssignmentTitle,
+    due_noAssignmentSubTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.due_noAssignmentSubTitle,
+    upcomingAssignmentBtn: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.upcomingAssignmentBtn,
+    upcoming_noAssignmentTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.upcoming_noAssignmentTitle,
+    upcoming_noAssignmentSubTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.upcoming_noAssignmentSubTitle,
+    completedAssignmentBtn: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completedAssignmentBtn,
+    completed_noAssignmentTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completed_noAssignmentTitle,
+    completed_noAssignmentSubTitle: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completed_noAssignmentSubTitle,
+    completeSearchIcon: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completeSearchIcon,
+    completeSearchTextBox: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completeSearchTextBox,
+    completeResultLbl: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.completeResultLbl,
+    assignmentName: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.assignmentName,
+    activityIcon: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.activityIcon,
+    assignmentActivityCount: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.assignmentActivityCount,
+    scoreIcon: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.scoreIcon,
+    scoreLabel: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.scoreLabel,
+    dateIcon: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.dateIcon,
+    dateLabel: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.dateLabel,
+    dueDaysPill: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.dueDaysPill,
+    activityProgressText: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.activityProgressText,
+    showActivitiesBtn: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.showActivitiesBtn,
+    collapsibleActvitiesLbl: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.collapsibleActvitiesLbl,
+    collapsibleScoreLbl: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.collapsibleScoreLbl,
+    collapsibleAttemptsLbl: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.collapsibleAttemptsLbl,
+    activityName: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.activityName,
+    activityStatus: selectorFile.css.ComproEngage.AssignmentListDetailsPage_student.activityStatus,
+
+
+    isInitialized: function () { 
+        var res;
+        logger.logInto(stackTrace.get());
+        action.waitForDocumentLoad();
+        res = {
+            pageStatus:  action.waitForDisplayed(this.assignmentDetailPageTitle),
+            appShellPage:appShellPage.isInitialized()};
+        return res; 
+    },
+
+    getData_assignmentListPage: function () {
+        logger.logInto(stackTrace.get());
+        var obj;
+        obj = {
+            assignmentDetailPageTitle:(action.getElementCount(this.assignmentDetailPageTitle) > 0) ? action.getText(this.assignmentDetailPageTitle) : null,
+            assignmentDetailPageSubtitle:(action.getElementCount(this.assignmentDetailPageSubtitle) > 0) ? action.getText(this.assignmentDetailPageSubtitle) : null,
+            dueAssignmentBtn:(action.getElementCount(this.dueAssignmentBtn) > 0) ? action.getText(this.dueAssignmentBtn) : null,
+            dueAssignmentCount:(action.getElementCount(this.dueAssignmentCount) > 0) ? action.getText(this.dueAssignmentCount) : null,
+            due_noAssignmentTitle:(action.getElementCount(this.due_noAssignmentTitle) > 0) ? action.getText(this.due_noAssignmentTitle) : null,
+            due_noAssignmentSubTitle:(action.getElementCount(this.due_noAssignmentSubTitle) > 0) ? action.getText(this.due_noAssignmentSubTitle) : null,
+            upcomingAssignmentBtn:(action.getElementCount(this.upcomingAssignmentBtn) > 0) ? action.getText(this.upcomingAssignmentBtn) : null,
+            upcoming_noAssignmentTitle:(action.getElementCount(this.upcoming_noAssignmentTitle) > 0) ? action.getText(this.upcoming_noAssignmentTitle) : null,
+            upcoming_noAssignmentSubTitle:(action.getElementCount(this.upcoming_noAssignmentSubTitle) > 0) ? action.getText(this.upcoming_noAssignmentSubTitle) : null,
+            completedAssignmentBtn:(action.getElementCount(this.completedAssignmentBtn) > 0) ? action.getText(this.completedAssignmentBtn) : null,
+            completed_noAssignmentTitle:(action.getElementCount(this.completed_noAssignmentTitle) > 0) ? action.getText(this.completed_noAssignmentTitle) : null,
+            completed_noAssignmentSubTitle:(action.getElementCount(this.completed_noAssignmentSubTitle) > 0) ? action.getText(this.completed_noAssignmentSubTitle) : null,
+            completeSearchIcon:(action.getElementCount(this.completeSearchIcon) > 0) ? action.waitForDisplayed(this.completeSearchIcon) : false,
+            completeSearchTextBox:(action.getElementCount(this.completeSearchTextBox) > 0) ? action.getText(this.completeSearchTextBox) : null,
+            completeResultLbl:(action.getElementCount(this.completeResultLbl) > 0) ? action.getText(this.completeResultLbl) : null,
+        }
+        console.log("obj - ",obj)
+        return obj; 
+    },
+
+    getData_assignmentList: function (assignmentNameName) {
+        logger.logInto(stackTrace.get());
+        var obj=[];
+        action.waitForDisplayed(this.assignmentName);
+        var list = action.findElements(this.assignmentName);
+        if (assignmentNameName) {
+            for (var i=0;i<list.length;i++){
+                if (action.getText(this.assignmentName + i) == assignmentNameName) {
+                    obj[0] = {
+                        assignmentName:(action.getElementCount(this.assignmentName+i+"]")  > 0) ? action.getText(this.assignmentName+i+"]")  : null,
+                        activityIcon:(action.getElementCount(this.activityIcon+i+"]") > 0) ? action.waitForDisplayed(this.activityIcon+i+"]")  : false,
+                        assignmentActivityCount:(action.getElementCount(this.assignmentActivityCount+i+"]")  > 0) ? action.getText(this.assignmentActivityCount+i+"]")  : null,
+                        scoreIcon:(action.getElementCount(this.scoreIcon+i+"]") > 0) ? action.waitForDisplayed(this.scoreIcon+i+"]")  : false,
+                        scoreLabel:(action.getElementCount(this.scoreLabel+i+"]")  > 0) ? action.getText(this.scoreLabel+i+"]")  : null,
+                        dateIcon:(action.getElementCount(this.dateIcon+i+"]") > 0) ? action.waitForDisplayed(this.dateIcon+i+"]")  : false,
+                        dateLabel:(action.getElementCount(this.dateLabel+i+"]")  > 0) ? action.getText(this.dateLabel+i+"]")  : null,
+                        dueDaysPill:(action.getElementCount(this.dueDaysPill+i+"]")  > 0) ? action.getText(this.dueDaysPill+i+"]")  : null,
+                        activityProgressText:(action.getElementCount(this.activityProgressText+i+"]")  > 0) ? action.getText(this.activityProgressText+i+"]")  : null,
+                        showActivitiesBtn:(action.getElementCount(this.showActivitiesBtn+i+"]")  > 0) ? action.getText(this.showActivitiesBtn+i+"]")  : null,
+                    }
+                break; 
+                }
+            } 
+        }
+        else {
+            for (var i=0;i<list.length;i++){
+                obj[i] = {
+                    assignmentName:(action.getElementCount(this.assignmentName+i+"]")  > 0) ? action.getText(this.assignmentName+i+"]")  : null,
+                    activityIcon:(action.getElementCount(this.activityIcon+i+"]") > 0) ? action.waitForDisplayed(this.activityIcon+i+"]")  : false,
+                    assignmentActivityCount:(action.getElementCount(this.assignmentActivityCount+i+"]")  > 0) ? action.getText(this.assignmentActivityCount+i+"]")  : null,
+                    scoreIcon:(action.getElementCount(this.scoreIcon+i+"]") > 0) ? action.waitForDisplayed(this.scoreIcon+i+"]")  : false,
+                    scoreLabel:(action.getElementCount(this.scoreLabel+i+"]")  > 0) ? action.getText(this.scoreLabel+i+"]")  : null,
+                    dateIcon:(action.getElementCount(this.dateIcon+i+"]") > 0) ? action.waitForDisplayed(this.dateIcon+i+"]")  : false,
+                    dateLabel:(action.getElementCount(this.dateLabel+i+"]")  > 0) ? action.getText(this.dateLabel+i+"]")  : null,
+                    dueDaysPill:(action.getElementCount(this.dueDaysPill+i+"]")  > 0) ? action.getText(this.dueDaysPill+i+"]")  : null,
+                    activityProgressText:(action.getElementCount(this.activityProgressText+i+"]")  > 0) ? action.getText(this.activityProgressText+i+"]")  : null,
+                    showActivitiesBtn:(action.getElementCount(this.showActivitiesBtn+i+"]")  > 0) ? action.getText(this.showActivitiesBtn+i+"]")  : null,
+                }
+            }
+        }
+        return obj; 
+        },
+
+    getData_activityList: function (collapsibleActvitiesLblName) {
+        logger.logInto(stackTrace.get());
+        var obj=[];
+        action.waitForDisplayed(this.collapsibleActvitiesLbl);
+        var list = action.findElements(this.collapsibleActvitiesLbl);
+        if (collapsibleActvitiesLblName) {for (var i=0;i<list.length;i++){
+        if (action.getText(this.collapsibleActvitiesLbl + i) == collapsibleActvitiesLblName) {
+        obj[0] = {
+        collapsibleActvitiesLbl:(action.getElementCount(this.collapsibleActvitiesLbl+i+"]")  > 0) ? action.getText(this.collapsibleActvitiesLbl+i+"]")  : null,
+        collapsibleScoreLbl:(action.getElementCount(this.collapsibleScoreLbl+i+"]")  > 0) ? action.getText(this.collapsibleScoreLbl+i+"]")  : null,
+        collapsibleAttemptsLbl:(action.getElementCount(this.collapsibleAttemptsLbl+i+"]")  > 0) ? action.getText(this.collapsibleAttemptsLbl+i+"]")  : null,
+        activityName:(action.getElementCount(this.activityName+i+"]")  > 0) ? action.getText(this.activityName+i+"]")  : null,
+        activityStatus:(action.getElementCount(this.activityStatus+i+"]")  > 0) ? action.getText(this.activityStatus+i+"]")  : null,
+        }
+        break; 
+        }
+        } 
+        }else{
+        for (var i=0;i<list.length;i++){
+        obj[i] = {
+        collapsibleActvitiesLbl:(action.getElementCount(this.collapsibleActvitiesLbl+i+"]")  > 0) ? action.getText(this.collapsibleActvitiesLbl+i+"]")  : null,
+        collapsibleScoreLbl:(action.getElementCount(this.collapsibleScoreLbl+i+"]")  > 0) ? action.getText(this.collapsibleScoreLbl+i+"]")  : null,
+        collapsibleAttemptsLbl:(action.getElementCount(this.collapsibleAttemptsLbl+i+"]")  > 0) ? action.getText(this.collapsibleAttemptsLbl+i+"]")  : null,
+        activityName:(action.getElementCount(this.activityName+i+"]")  > 0) ? action.getText(this.activityName+i+"]")  : null,
+        activityStatus:(action.getElementCount(this.activityStatus+i+"]")  > 0) ? action.getText(this.activityStatus+i+"]")  : null,
+        }
+        }
+        }
+        return obj; 
+    }
+
+}
+
---
+++ engageAutomation/test/engageExperienceApp/assignmentListDetails_students.test.js
@@ -0,0 +1,97 @@
+"use strict";
+var assignmentListDetailsPage_student = require('../../pages/engageExperienceApp/AssignmentListDetailsPage_student.page.js');
+var appShell = require('../../pages/engageExperienceApp/appShell.page');
+const { confirmPassword_input } = require('../../pages/engageExperienceApp/settings.page.js');
+var sts;
+
+module.exports = {
+
+   //Validate the blank Due assignment page
+   ENG_ASSLISTDETAILS_STU_TC_1: function (testdata) {
+      sts = assignmentListDetailsPage_student.getData_assignmentListPage()
+      assertion.assertEqual(sts.due_noAssignmentTitle, testdata.due_noAssignmentTitle, "due_noAssignmentTitle Label status mismatch");
+      assertion.assertEqual(sts.due_noAssignmentSubTitle, testdata.due_noAssignmentSubTitle, "due_noAssignmentSubTitle Label is avialable");
+
+   },
+
+   //Validate the blank Upcoming assignment page
+   ENG_ASSLISTDETAILS_STU_TC_2: function (testdata) {
+      sts = assignmentListDetailsPage_student.getData_assignmentListPage()
+      assertion.assertEqual(sts.upcoming_noAssignmentTitle, testdata.upcoming_noAssignmentTitle, "upcoming_noAssignmentTitle Label status mismatch");
+      assertion.assertEqual(sts.upcoming_noAssignmentSubTitle, testdata.upcoming_noAssignmentSubTitle, "upcoming_noAssignmentSubTitle Label is avialable");
+   },
+
+   //Validate the blank Completed assignment page
+   ENG_ASSLISTDETAILS_STU_TC_3: function (testdata) {
+      sts = assignmentListDetailsPage_student.getData_assignmentListPage()
+      assertion.assertEqual(sts.completed_noAssignmentTitle, testdata.completed_noAssignmentTitle, "completed_noAssignmentTitle Label status mismatch");
+      assertion.assertEqual(sts.completed_noAssignmentSubTitle, testdata.completed_noAssignmentSubTitle, "completed_noAssignmentSubTitle Label is avialable");
+   },
+
+   //Validate the click on upcoming assignment
+   ENG_ASSLISTDETAILS_TC_1: function (testdata) {
+      sts = assignmentListDetailsPage.click_upcoming_lbl();
+      assertion.assertEqual(sts, true, "Select tab error");
+      sts = appShell.getTabsListData();
+      assertion.assertEqual(sts.selected.includes(testdata.tabList[1]), true, "Selected tab mismatch");
+   },
+
+   //Validate the click on past assignment
+   ENG_ASSLISTDETAILS_TC_2: function (testdata) {
+      sts = assignmentListDetailsPage.click_past_lbl();
+      assertion.assertEqual(sts, true, "Select tab error");
+      sts = appShell.getTabsListData();
+      assertion.assertEqual(sts.selected.includes(testdata.tabList[2]), true, "Selected tab mismatch");
+   },
+
+   //Validate the click on in-progress assignment
+   ENG_ASSLISTDETAILS_TC_3: function (testdata) {
+      sts = assignmentListDetailsPage.click_inProgress_lbl();
+      assertion.assertEqual(sts, true, "Select tab error");
+      sts = appShell.getTabsListData();
+      assertion.assertEqual(sts.selected.includes(testdata.tabList[0]), true, "Selected tab mismatch");
+   },
+
+
+
+   //Validate the content of assignment list details page
+   ENG_ASSLISTDETAILS_TC_7: function (testdata) {
+      sts = assignmentListDetailsPage.getData_assignmentListPage()
+      assertion.assertEqual(sts.Assignment_title, testdata.Assignment_title, "Assignment_title status mismatch");
+      assertion.assertEqual(sts.Assignment_subtitle, testdata.Assignment_subtitle, "Assignment_subtitle Label status mismatch");
+      assertion.assertEqual(sts.addNew_btn, testdata.addNew_btn, "addNew_btn Label is avialable");
+      assertion.assertEqual(sts.inProgress_lbl, testdata.inProgress_lbl, "inProgress_lbl status mismatch");
+      assertion.assertEqual(sts.upcoming_lbl, testdata.upcoming_lbl, "upcoming_lbl status mismatch");
+      assertion.assertEqual(sts.past_lbl, testdata.past_lbl, "past_lbl Label status mismatch");
+   },
+
+   //validate the details of assignment card
+   ENG_ASSLISTDETAILS_TC_8: function (testdata) {
+      sts = assignmentListDetailsPage.getData_assignmentCard(testdata)
+      assertion.assertEqual(sts[0].assignmentName, testdata, "Assignment name is mismatch");
+   },
+
+   ENG_ASSLISTDETAILS_TC_9: function (testdata) {
+      sts = assignmentListDetailsPage.click_editAssignment_btn(testdata)
+      assertion.assertEqual(sts.pageStatus, true, "Edit Assignment page status mismatch");
+   },
+
+   ENG_ASSLISTDETAILS_TC_10: function (testdata) {
+      sts = assignmentListDetailsPage.click_cloneAssignment_btn(testdata)
+      assertion.assertEqual(sts.pageStatus, true, "Create Assignment page status mismatch");
+   },
+
+   ENG_ASSLISTDETAILS_TC_11: function (testdata) {
+      sts = assignmentListDetailsPage.click_deleteAssignment_btn(testdata)
+      //ENG_ASSDETAILS_TC_3
+      assertion.assertEqual(sts.deletedialogueHeader, testdata.deletedialogueHeader, "deletedialogueHeader is mismatch");
+      assertion.assertEqual(sts.cancel_btn, testdata.cancel_btn, "cancel_btn is  mismatch");
+      assertion.assertEqual(sts.delete_btn, testdata.delete_btn, "delete_btn is  mismatch");
+   },
+
+   ENG_ASSLISTDETAILS_TC_12: function (testdata) {
+      sts = assignmentListDetailsPage.click_viewProgress_btn(testdata)
+      assertion.assertEqual(sts.pageStatus, true, "Assignment Details page status mismatch");
+   },
+
+}
